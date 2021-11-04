c9a2bb8e8881da4a4007baa28a0aa2e70dc8b14b
Fabricio Calvanese Augusto <fabriciocalvanese@gmail.com>
2021-11-04 14:20:05 -0300

    Mudanças na Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-    //val alunoTeste = Estudante("Julio", "Cesar", 1212)
-    val alunoTeste = Estudante("Julio", "Cesar", 1212, 70.0, 7)
-    val daniel = Estudante("Daniel", "Pereira", 1212, 90.0, 7)
-    val alunoTerceiro = Estudante("Daniel", "Pereira", 1212, 50.0, 7)
+    //val alunoTeste = Estudante("Fabricio", "Calvanese", 1212)
+    val alunoTeste = Estudante("Fabricio", "Calvanese", 3212, 70.0, 7)
+    val daniel = Estudante("Rogério", "da Silva", 1212, 90.0, 7)
+    val alunoTerceiro = Estudante("Rogério", "da Silva", 1212, 50.0, 7)
 
 
-    val cursoTeste = Cursos("Filosofia", 9)
+    val cursoTeste = Cursos("Japonês", 9)
 
     //alunoTeste.mostraInformacoesAluno()
 
